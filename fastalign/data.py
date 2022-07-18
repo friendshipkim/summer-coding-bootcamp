4aa9fb463c3b6000f567928584e188bee0aa46de
Sasha Rush <srush.research@gmail.com>
2022-07-18 17:22:14 -0400

    up

---
+++ fastalign/data.py
@@ -0,0 +1,13 @@
+from datasets import load_dataset
+import torch
+# dataset = load_dataset("iwslt2017", 'iwslt2017-en-de')
+
+# dict_en = {}
+# dict_de
+
+# for pair in dataset["train"]["translation"][0]:
+#     de = pair["de"]
+#     en = pair["en"]
+#     de.split(), en.split()
+
+
